a8fa98f1491951729a150409c30cde5ad1652594
Your NameVote4Pedrobr <pedroivan1910@gmail.com>
2021-01-30 19:51:47 -0300

    teste number 2

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(MaterialApp(title: "Contador de pessoas", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _infotext = "Pode entrar!";
+
+  void _changePeople(int delta) {
+    setState(() {
+      _people += delta;
+      if (_people < 0) {
+        _infotext = "Mundo invertido???";
+      } else if (_people > 12) {
+        _infotext = "Estabelecimento lotado!";
+      } else {
+        _infotext = "Pode entrar!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: <Widget>[
+      Image.asset(
+        "images/restaurant.jpg",
+        fit: BoxFit.cover,
+        height: 1000.0,
+      ),
+      Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+        Text(
+          "Pessoas: $_people",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(padding: EdgeInsets.all(10.0)),
+            FlatButton(
+                onPressed: () {
+                  _changePeople(1);
+                },
+                child: Text(
+                  "+1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+                )),
+            Padding(padding: EdgeInsets.all(10.0)),
+            FlatButton(
+                onPressed: () {
+                  _changePeople(-1);
+                },
+                child: Text(
+                  "-1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+                ))
+          ],
+        ),
+        Text(
+          _infotext,
+          style: TextStyle(
+              color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30.0),
+        )
+      ])
+    ]);
+  }
+}
